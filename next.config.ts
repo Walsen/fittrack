af6beaaec15542079742ff2e776f80ab869b3d23
Sergio Rodriguez <sergio@ffactory.io>
2026-04-05 03:34:34 -0400

    fix: exclude CDK and Amplify backend packages from Next.js build traces

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    outputFileTracingExcludes: {
+      "*": [
+        "node_modules/aws-cdk-lib/**",
+        "node_modules/aws-cdk/**",
+        "node_modules/@aws-amplify/data-construct/**",
+        "node_modules/@aws-amplify/graphql-api-construct/**",
+        "node_modules/constructs/**",
+        "node_modules/esbuild/**",
+        "node_modules/typescript/**",
+      ],
+    },
+  },
   webpack: (config, { dev }) => {
     if (!dev) {
       if (!config.optimization) {
